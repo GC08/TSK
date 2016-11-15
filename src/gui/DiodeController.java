14dc8341836918625fade316f835332e8b987bd7
GC08 <gniewekciolek@gmail.com>
2016-11-15 11:56:39 +0100

    animowany wykres źródła napięciowego

--- src/gui/DiodeController.java
+++ src/gui/DiodeController.java
@@ -1,15 +1,22 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 import electric_circuit.DiodePN;
 import electric_circuit.DiodeSi;
 import electric_circuit.VoltageSource;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.*;
+import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -29,11 +36,12 @@ public class DiodeController implements Initializable {
 	@FXML
 	private Label label;
 
-	@FXML 
+	@FXML
 	private LineChart<Number, Number> lc;
-	
-	private VoltageSource source = new VoltageSource(1, VoltageSource.Type.SINUS, 0, 1);
+
+	private VoltageSource source = new VoltageSource(1, VoltageSource.Type.SINUS, 0, 0.25);
 	private DiodePN diode = new DiodeSi();
+	private ArrayList sourceVoltage = new ArrayList<Double>();
 
 	@FXML
 	private void handleButtonAction(ActionEvent event) {
@@ -43,52 +51,68 @@ public class DiodeController implements Initializable {
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		Thread one = new Thread() {
-		    public void run() {
-		        try {
-		        	double time = 0;
-		        	
-		        	while(true){
-		        		UpdateGraphs(time);
-		        		time += 0.1;
-			            Thread.sleep(100);
-		        	}
-		        } catch(InterruptedException v) {
-		            System.out.println(v);
-		        }
-		    }  
+		for (int i = 0; i < 100; i++) {
+			sourceVoltage.add(0);
+		}
+		lc.getXAxis().setAutoRanging(true);
+		lc.getYAxis().setAutoRanging(true);
+
+		lc.setCreateSymbols(false);
+		lc.setTitle("Sourrce Voltage");
+		XYChart.Series series = new XYChart.Series();
+		series.setName("Voltage");
+		serieFromList(series, sourceVoltage);
+		XYChart.Series series2 = new XYChart.Series();
+		series2.getData().add(new XYChart.Data(0, 3));
+
+		lc.getData().add(0, series);
+		lc.getData().add(series2);
+		
+		Task task = new Task<Void>() {
+			@Override
+			public Void call() throws Exception {
+				double time = 0;
+				while (true) {
+					time += 0.100;
+					final double finalTime = time;
+					Platform.runLater(new Runnable() {
+						@Override
+						public void run() {
+							sourceVoltage.remove(0);
+							sourceVoltage.add(source.U(finalTime));
+							XYChart.Series series = new XYChart.Series();
+							series.setName("Voltage");
+							serieFromList(series, sourceVoltage);
+							lc.getData().set(0, series);
+						}
+					});
+					Thread.sleep(100);
+				}
+			}
 		};
-
-		one.start();
-		
-		// NumberAxis xAxis = new NumberAxis();
-		// NumberAxis yAxis = new NumberAxis();
-		// xAxis.setLabel("Number of Month");
-		// // creating the chart
-		// lineChart = new LineChart<Number, Number>(xAxis, yAxis);
-		//
-		// lineChart.setTitle("Stock Monitoring, 2010");
-		// // defining a series
-		// XYChart.Series series = new XYChart.Series();
-		// series.setName("My portfolio");
-		// // populating the series with data
-		// series.getData().add(new XYChart.Data(1, 23));
-		// series.getData().add(new XYChart.Data(2, 14));
-		// series.getData().add(new XYChart.Data(3, 15));
-		// series.getData().add(new XYChart.Data(4, 24));
-		// series.getData().add(new XYChart.Data(5, 34));
-		// series.getData().add(new XYChart.Data(6, 36));
-		// series.getData().add(new XYChart.Data(7, 22));
-		// series.getData().add(new XYChart.Data(8, 45));
-		// series.getData().add(new XYChart.Data(9, 43));
-		// series.getData().add(new XYChart.Data(10, 17));
-		// series.getData().add(new XYChart.Data(11, 29));
-		// series.getData().add(new XYChart.Data(12, 25));
-		// lineChart.getData().add(series);
+		Thread th = new Thread(task);
+		th.setDaemon(true);
+		th.start();
 	}
 
-	protected void UpdateGraphs(double time) {
-		
+	private void serieFromList(Series series, ArrayList list) {
+		for (int i = 0; i < list.size(); i++) {
+			series.getData().add(new XYChart.Data(i, list.get(i)));
+		}
 	}
+//
+//	protected void UpdateGraphs(double time) {
+//		sourceVoltage.remove(0);
+//		sourceVoltage.add(0);
+//		XYChart.Series series = new XYChart.Series();
+//		series.setName("Voltage");
+//		serieFromList(series, sourceVoltage);
+//
+//		// this.lc.getData().retainAll();
+//		this.lc.getData().add(series);
+//		// lc.getData().get(0).getData().add(10,new XYChart.Data(12, 25));
+//		// XYChart.Data xd= new XYChart.Data(12, 25);
+//		// xd.getNode().(xd.getXValue() - 1);
+//	}
 
 }
